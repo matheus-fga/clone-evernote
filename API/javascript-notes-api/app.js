a152447824322a86a0d1fe734cc4ad6b483866bd
Matheus Silva <matheussilva.fga@gmail.com>
2022-05-31 15:56:25 -0300

    Creating routes to create a note

--- API/javascript-notes-api/app.js
+++ API/javascript-notes-api/app.js
@@ -5,7 +5,8 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 require('./config/database');
 
-var users = require('./app/routes/users');
+var usersRouter = require('./app/routes/users');
+var notesRouter = require('./app/routes/notes');
 
 var app = express();
 
@@ -20,7 +21,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/users', users);
+app.use('/users', usersRouter);
+app.use('/notes', notesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
