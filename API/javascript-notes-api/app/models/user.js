adc8a135c7ef1ccc24a9e39491a7ed4a6b466f44
Matheus Silva <matheussilva.fga@gmail.com>
2022-05-31 14:44:13 -0300

    Creating user login route and token with JWT

--- API/javascript-notes-api/app/models/user.js
+++ API/javascript-notes-api/app/models/user.js
@@ -22,4 +22,14 @@ userSchema.pre('save', function (next) {
   }
 });
 
+userSchema.methods.isCorrectPassword = function (password, callback) {
+  bcrypt.compare(password, this.password, function(error, same) {
+    if(error)
+      callback(error);
+    else {
+      callback(error, same);
+    }
+  });
+};
+
 module.exports = mongoose.model("User", userSchema);
