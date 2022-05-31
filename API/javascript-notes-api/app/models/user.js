ce7fbb17c7e4f1beaa23b1e40ee8507ba197c395
Matheus Silva <matheussilva.fga@gmail.com>
2022-05-31 10:52:50 -0300

    Hashing the password with bcrypt

--- API/javascript-notes-api/app/models/user.js
+++ API/javascript-notes-api/app/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 let userSchema = new mongoose.Schema({
   name: String,
@@ -6,6 +7,19 @@ let userSchema = new mongoose.Schema({
   password: {type: String, required: true},
   created_at: { type: Date, default: Date.now },
   updated_at: { type: Date, default: Date.now },
+});
+
+userSchema.pre('save', function (next) {
+  if (this.isNew || this.isModified('password')) {
+    bcrypt.hash(this.password, 10, (error, hashedPassword) => {
+      if(error)
+        next(error);
+      else {
+        this.password = hashedPassword;
+        next();
+      }
+    })
+  }
 });
 
 module.exports = mongoose.model("User", userSchema);
