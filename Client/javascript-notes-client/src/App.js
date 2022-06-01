5bb0ccfb5c766d81a7dcd60772f6c58ddbdc8cc9
Matheus Silva <matheussilva.fga@gmail.com>
2022-06-01 16:01:53 -0300

    Creating structure and global style of the project

--- Client/javascript-notes-client/src/App.js
+++ Client/javascript-notes-client/src/App.js
@@ -1,5 +1,14 @@
-import './App.css';
+import './App.scss';
+import { Notification, Section } from 'rbx';
 
-const App = () => <p>JavaScript Notes</p>
+const App = () => (
+  <div className='App'>
+    <Section>
+      <Notification color='success'>
+        JavaScript Notes!
+      </Notification>
+    </Section>
+  </div>
+)
 
 export default App;
