e5b1c12c256e59ed6db9ed218c0095f1440e914a
Matheus Silva <matheussilva.fga@gmail.com>
2022-06-02 16:08:32 -0300

    Creating user login screen

--- Client/javascript-notes-client/src/screens/auth/login/index.js
+++ Client/javascript-notes-client/src/screens/auth/login/index.js
@@ -1,5 +1,45 @@
 import React, { Fragment } from "react";
+import Header from "../../../components/header";
+import { Column, Section, Title, Container, Card } from "rbx";
+import logoImage from "../../../assets/images/logo.png";
+import LoginForm from "../../../components/forms/login";
+import "../../../styles/auth.scss";
 
-const LoginScreen = () => <Fragment>Hello Word</Fragment>;
+const LoginScreen = () => (
+  <Fragment>
+    <Header />
+    <Section size="small" className="auth">
+      <Container>
+        <Column.Group centered>
+          <Column size={4}>
+            <Card>
+              <Card.Content>
+                <Section>
+                  <Column.Group centered>
+                    <Column size={12}>
+                      <img src={logoImage} />
+                    </Column>
+                  </Column.Group>
+
+                  <Column.Group>
+                    <Column size={12}>
+                      <Title
+                        size={6}
+                        className="has-text-grey has-text-centered"
+                      >
+                        Your notes on the cloud
+                      </Title>
+                    </Column>
+                  </Column.Group>
+                  <LoginForm />
+                </Section>
+              </Card.Content>
+            </Card>
+          </Column>
+        </Column.Group>
+      </Container>
+    </Section>
+  </Fragment>
+);
 
 export default LoginScreen;
