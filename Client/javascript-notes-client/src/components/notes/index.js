debb10e25745da87460ba613aff395b7d859150e
Matheus Silva <matheussilva.fga@gmail.com>
2022-06-03 13:54:49 -0300

    Adding a sidebar menu on notes screen

--- Client/javascript-notes-client/src/components/notes/index.js
+++ Client/javascript-notes-client/src/components/notes/index.js
@@ -1,6 +1,33 @@
 import React, { Fragment, useEffect, useState } from "react";
+import { Column } from "rbx";
+import { push as Menu } from "react-burger-menu";
 
 function Notes(props) {
-  return <Fragment>Notes...</Fragment>;
+  return (
+    <Fragment>
+      <Column.Group className="notes" id="notes">
+        <Menu
+          pageWrapId={"notes-editor"}
+          isOpen={props.isOpen}
+          onStateChange={(state) => props.setIsOpen(state.isOpen)}
+          disableAutoFocus
+          outerContainerId={"notes"}
+          customBurgerIcon={false}
+          customCrossIcon={false}
+        >
+          <Column.Group>
+            <Column size={10} offset={1}>
+              Search...
+            </Column>
+          </Column.Group>
+          <p>List...</p>
+        </Menu>
+
+        <Column size={12} className="notes-editor" id="notes-editor">
+          Editor...
+        </Column>
+      </Column.Group>
+    </Fragment>
+  );
 }
 export default Notes;
--- Client/javascript-notes-client/src/screens/notes/index/index.js
+++ Client/javascript-notes-client/src/screens/notes/index/index.js
@@ -1,13 +1,17 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import HeaderLogged from "../../../components/headers/header_logged";
 import Notes from "../../../components/notes";
 import "../../../styles/notes.scss";
 
-const NotesScreen = () => (
-  <Fragment>
-    <HeaderLogged />
-    <Notes />
-  </Fragment>
-);
+const NotesScreen = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Fragment>
+      <HeaderLogged setIsOpen={setIsOpen} />
+      <Notes setIsOpen={setIsOpen} isOpen={isOpen} />
+    </Fragment>
+  );
+};
 
 export default NotesScreen;
