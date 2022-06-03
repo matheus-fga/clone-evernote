7937ee6827e5fb7305753b8231767f85af5b23c4
Matheus Silva <matheussilva.fga@gmail.com>
2022-06-03 11:53:14 -0300

    Creanting notes component and style

--- Client/javascript-notes-client/src/screens/notes/index/index.js
+++ Client/javascript-notes-client/src/screens/notes/index/index.js
@@ -1,9 +1,12 @@
 import React, { Fragment } from "react";
 import HeaderLogged from "../../../components/headers/header_logged";
+import Notes from "../../../components/notes";
+import "../../../styles/notes.scss";
 
 const NotesScreen = () => (
   <Fragment>
     <HeaderLogged />
+    <Notes />
   </Fragment>
 );
 
