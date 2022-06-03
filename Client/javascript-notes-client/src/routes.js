7bb1f73e173b7f0ed51ad8dd36951f1deaf7e01b
Matheus Silva <matheussilva.fga@gmail.com>
2022-06-03 10:42:25 -0300

    Creating auth component to protect routes

--- Client/javascript-notes-client/src/routes.js
+++ Client/javascript-notes-client/src/routes.js
@@ -5,6 +5,7 @@ import NotesScreen from "./screens/notes/index";
 import RegisterScreen from "./screens/auth/register";
 import LoginScreen from "./screens/auth/login";
 import UsersEditScreen from "./screens/users/edit";
+import PrivateRoute from "./components/auth";
 
 const AppRoutes = () => (
   <Router>
@@ -12,7 +13,9 @@ const AppRoutes = () => (
       <Route exact path="/" element={<HomeScreen />} />
       <Route exact path="/register" element={<RegisterScreen />} />
       <Route exact path="/login" element={<LoginScreen />} />
-      <Route exact path="/notes" element={<NotesScreen />} />
+      <Route exact path="/notes" element={<PrivateRoute />}>
+        <Route exact path="/notes" element={<NotesScreen />} />
+      </Route>
       <Route exact path="/users/edit" element={<UsersEditScreen />} />
     </Routes>
   </Router>
