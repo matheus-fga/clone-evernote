3a7f8a3b189e2fd0bc1f3793f2ed6d156b680109
Matheus Silva <matheussilva.fga@gmail.com>
2022-06-07 20:57:42 -0300

    Creanting routes and middleware to update and delete a user

--- API/javascript-notes-api/app/models/user.js
+++ API/javascript-notes-api/app/models/user.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 let userSchema = new mongoose.Schema({
   name: String,
@@ -9,23 +9,28 @@ let userSchema = new mongoose.Schema({
   updated_at: { type: Date, default: Date.now },
 });
 
-userSchema.pre('save', function (next) {
-  if (this.isNew || this.isModified('password')) {
+userSchema.pre("save", function (next) {
+  if (this.isNew || this.isModified("password")) {
     bcrypt.hash(this.password, 10, (error, hashedPassword) => {
-      if(error)
-        next(error);
+      if (error) next(error);
       else {
         this.password = hashedPassword;
         next();
       }
-    })
+    });
   }
 });
 
+userSchema.pre("remove", function (next) {
+  const Notes = mongoose.model("Note");
+  Notes.deleteMany({ author: this._id })
+    .then(() => next())
+    .catch((error) => next(error));
+});
+
 userSchema.methods.isCorrectPassword = function (password, callback) {
-  bcrypt.compare(password, this.password, function(error, same) {
-    if(error)
-      callback(error);
+  bcrypt.compare(password, this.password, function (error, same) {
+    if (error) callback(error);
     else {
       callback(error, same);
     }
--- API/javascript-notes-api/app/routes/users.js
+++ API/javascript-notes-api/app/routes/users.js
@@ -1,41 +1,74 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const User = require('../models/user');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 const secret = process.env.JWT_TOKEN;
+const withAuth = require("../middlewares/auth");
 
-router.post('/register', async (req, res) => {
+router.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
-  
+
   try {
     const user = new User({ name, email, password });
     await user.save();
     res.status(200).json(user);
   } catch (error) {
-    res.status(500).json({ error: 'Error registering new user' }); 
+    res.status(500).json({ error: "Error registering new user" });
   }
 });
 
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
   try {
     let user = await User.findOne({ email });
-    if(!user)
-      res.status(401).json({ error: 'Incorrect email or password' });
+    if (!user) res.status(401).json({ error: "Incorrect email or password" });
     else {
       user.isCorrectPassword(password, function (error, same) {
-        if(!same)
-          res.status(401).json({ error: 'Incorrect email or password' });
+        if (!same)
+          res.status(401).json({ error: "Incorrect email or password" });
         else {
-          const token = jwt.sign({email}, secret, { expiresIn: '1d' });
+          const token = jwt.sign({ email }, secret, { expiresIn: "1d" });
           res.json({ user: user, token: token });
         }
       });
     }
   } catch (error) {
-    res.status(500).json({ error: 'Internal Error, please try again' }); 
+    res.status(500).json({ error: "Internal Error, please try again" });
+  }
+});
+
+router.put("/", withAuth, async (req, res) => {
+  const { name, email } = req.body;
+
+  try {
+    let user = await User.findByIdAndUpdate(
+      req.user._id,
+      {
+        $set: {
+          name: name,
+          email: email,
+          updated_at: Date.now(),
+        },
+      },
+      { upsert: true, new: true }
+    );
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: "Error updating user" });
+  }
+});
+
+router.delete("/", withAuth, async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    let userToDelete = await User.findById(req.user._id);
+    userToDelete.delete();
+    res.status(204).json();
+  } catch (error) {
+    res.status(500).json({ error: "Error to remove user" });
   }
 });
 
